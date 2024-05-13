b4e8cd2e80177af153984b6bb342eabb4227fcdf
Eduardo Cabrera <eduardo.cabrera@rimac.com.pe>
2024-05-13 11:41:11 -0500

    desarrollo 8-15 Printing Models

---
+++ chapter_08/PracticaPersonal/8-15 Printing Models.py
@@ -0,0 +1,8 @@
+import printing_functions as pf
+lista_por_hacer = ['modelo1', 'modelo2', 'Modelo3']
+lista_hecho = []
+pf.print_models(lista_por_hacer,lista_hecho)
+
+
+#print(lista_por_hacer)
+#print(lista_hecho)
