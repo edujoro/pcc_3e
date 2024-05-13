768f2c672c97b606ea9cbf625398120e7e31dad3
Eduardo Cabrera <eduardo.cabrera@rimac.com.pe>
2024-05-13 11:52:42 -0500

    Styling Functions

---
+++ chapter_08/PracticaPersonal/8-17 Styling Functions.py
@@ -0,0 +1,13 @@
+def haz_camisa(talla, mensaje):
+    #muestra un mensaje ordenado esperado para la persona que lo lea
+    print(f'La camisa es de talla {talla} y mensaje es {mensaje}')
+
+
+def show_messages(mensajes):
+    for mensaje in mensajes:
+        print(mensaje)
+
+
+def show_messages(mensajes):
+    for mensaje in mensajes:
+        print(mensaje)
