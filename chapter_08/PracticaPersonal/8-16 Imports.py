4a04413cb5ef002d5cb60293de7755a12758cf17
Eduardo Cabrera <eduardo.cabrera@rimac.com.pe>
2024-05-13 11:45:37 -0500

    8-16 Imports

---
+++ chapter_08/PracticaPersonal/8-16 Imports.py
@@ -0,0 +1,7 @@
+import printing_functions
+
+from printing_functions import print_models
+from printing_functions import print_models as pm
+from printing_functions import *
+
+import printing_functions as pf
