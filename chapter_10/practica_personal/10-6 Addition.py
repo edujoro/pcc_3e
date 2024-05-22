24343aecf62a052dec25dee8d30f29549c126628
edujo <eduardo5288@gmail.com>
2024-05-21 19:31:16 -0500

    10-6 Adition

---
+++ chapter_10/practica_personal/10-6 Addition.py
@@ -0,0 +1,8 @@
+try:
+    num1 = int(input('Enter first number: '))
+    num2 = int(input('Enter second number: '))
+except ValueError:
+    print('Invalid input')
+else:
+    suma = num1 + num2
+    print('The sum of these numbers is: ', sum)
